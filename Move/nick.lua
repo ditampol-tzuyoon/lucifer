f4be2da1d0244f47bf17666bd1c7e018ea110468
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2024-06-20 17:59:02 +0800

    Create nick.lua

---
+++ Move/nick.lua
@@ -0,0 +1,64 @@
+LogoPartai = {
+    "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/Erine/1.jpg",
+    "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/Erine/2.jpg",
+    "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/Erine/3.jpg",
+    "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/Erine/4.jpg",
+    "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/Erine/5.jpg",
+    "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/Erine/6.jpg",
+    "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/Erine/7.jpg",
+    "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/Erine/8.jpg",
+    "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/Erine/9.jpg",
+    "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/Erine/10.jpg",
+    "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/Erine/11.jpg",
+    "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/Erine/12.jpg",
+    "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/Erine/13.jpg"
+}
+
+Odirrrr = "https://raw.githubusercontent.com/ditampol-tzuyoon/surfer/main/Image%20Animasi.gif"
+LinkStore = "https://discord.gg/UG6MNdqtTa"
+IP_Server_OD = "47.253.194.85"
+ListPembeli = {
+    "422670528483033088"
+}
+
+function ohdsay(logger, TypeHook, Pings)
+
+    Lopar = LogoPartai[math.random(1, #LogoPartai)]
+    Judulz = "Dropped Multi Item Lucifer || "..#ListPembeli.." Users. || Author Ohdear_"
+
+    if bot.status == 1 then
+        Warna = 7405312
+        statzBot = "<a:Onlen:1206807819370758204>"
+    else
+        Warna = 16711680
+        statzBot = "<a:Oflen:1206807838996045844>"
+    end
+
+    if Pings then
+        Ment = " | <@"..userdc..">"
+    else
+        Ment = ""
+    end
+    
+    if TypeHook == "PingHook" then
+        wh = Webhook.new(PingHook)
+        wh.content = statzBot.." | "..logger..Ment
+        wh.username = bot.name
+        wh:send()
+    elseif TypeHook == "MainHook" then
+        wh = Webhook.new(MainWebhook)
+        wh.content = Ment
+        wh.username = bot.name
+        wh.embed1.use = true
+        wh.embed1.description = logger
+        wh.embed1.color = Warna
+        wh.embed1.footer.text = "Dropped Items by Ohdear_\n"..IniWaktu()
+        wh.embed1.image = Odirrrr
+        wh.embed1.thumbnail = Lopar
+        wh.embed1.author.name = Judulz
+        wh.embed1.author.url = LinkStore
+        wh.embed1:addField(":crown: Bot Name", bot.name.." | **Lv"..bot.level.."** | "..getPing().."ms", false)
+        wh.embed1:addField(":package: List Dropped", ShowItem, false)
+        wh:edit(MainIDMessage)
+    end
+end
