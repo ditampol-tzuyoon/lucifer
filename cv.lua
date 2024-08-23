b7bffc290504c5c1c557157264686f139c9039c2
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2024-08-23 20:14:10 +0800

    Update cv.lua

--- cv.lua
+++ cv.lua
@@ -21,7 +21,6 @@ function StatusBot(detail)
         [BotStatus.http_block] = "HTTP Block",
         [BotStatus.mod_entered] = "Mod Entered",
         [BotStatus.changing_subserver] = "Subserver",
-        [BotStatus.bad_gateway] = "Bad Gateway",
         [BotStatus.server_issue] = "Server Issue",
         [BotStatus.retrieving_token] = "Retrieving Token",
         [BotStatus.logon_fail] = "Login Fail",
