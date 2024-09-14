2ae67374a4cd3d3964b52b998013bc4949f7f71f
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2024-09-15 06:25:41 +0800

    Update buyer.lua

--- Quest/buyer.lua
+++ Quest/buyer.lua
@@ -1,22 +1,5 @@
 UsernameBuyer = {
   "erine",
-  "slava",
-  "ryyzu",
-  "alditaherjelek",
-  "XcoBar666",
-  "raki",
-  "jaryuuu1",
-  "babi2005",
-  "octadamulti",
-  "arielpelu123",
-  "reyvaltanq2",
-  "ROBYAN26",
-  "meotd201",
-  "barunih4",
-  "luciriky1",
-  "nanah1",
-  "Lampu4",
-  "dairoh1"
 }
 
 ListBuyer = {
