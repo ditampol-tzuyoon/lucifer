69ef78f748eb4ddc3cb133ca53bdad35b2f70d15
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2024-10-17 12:46:00 +0800

    Update own.lua

--- Rotasi/own.lua
+++ Rotasi/own.lua
@@ -12,5 +12,23 @@ delayrandomworld = 10000
 nLettPNB = 6
 Limit_BotInfo = 12
 PNBinFarm = false
+autobuypack = true
 
-if getUsername() == "oline5" then autobuypack = false end
+-- if getUsername() == "oline5" then autobuypack = false end
+
+hargapack = 150000                -- Pack Price
+namapack = "incognito_hat"     -- Pack Name
+MinItemPack = 1                 -- Bot will dropping the pack when he have X MinItemPack or more
+iditempack = {5706, 14832}              -- Put all your id item pack in here
+maxbuy = 2                      -- Bot will try to buying X times if have more gems
+MinGems = hargapack                 -- Minimal Gems for Buying Pack
+
+IgnorePack = {}
+for _,pack in pairs(iditempack) do
+    if not pengecualian[pack] then
+        pengecualian[pack]=true
+    end
+    if not IgnorePack[pack] then
+        IgnorePack[pack] = true
+    end
+end
