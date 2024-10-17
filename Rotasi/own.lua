4a89c22ff29214ce1c5e482bb89593ca1beafac2
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2024-10-17 11:44:34 +0800

    Update own.lua

--- Rotasi/own.lua
+++ Rotasi/own.lua
@@ -4,7 +4,6 @@ delayplant = 150
 delayht = 150
 TargetGems = 999999999
 setoran = 75
-autobuypack = true
 JedaHW = 10
 TilePNB = 5                 -- (1 - 5)
 idVial = 8542
@@ -13,3 +12,5 @@ delayrandomworld = 10000
 nLettPNB = 6
 Limit_BotInfo = 12
 PNBinFarm = false
+
+if getUsername() == "oline5" then autobuypack = false end
