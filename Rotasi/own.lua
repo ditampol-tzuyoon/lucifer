0f463b33ab9a949dfd6feef2803e5693dde68965
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2024-10-18 12:14:09 +0800

    Update own.lua

--- Rotasi/own.lua
+++ Rotasi/own.lua
@@ -17,5 +17,20 @@ autobuypack = true
 DelayRecon = 90000              -- Interval Reconnecting
 DelayBadServer = 120000
 
--- if getUsername() == "oline5" then autobuypack = false end
-if findItem(112) >= 140000 then PNBinFarm = true end
+ID_AcuanDrop_Pack = 13           -- ID Foreground or Background for Drop Pack
+hargapack = 15000                -- Pack Price
+namapack = "star_supplies"       -- Pack Name
+MinItemPack = 10                 -- Bot will dropping the pack when he have X MinItemPack or more
+iditempack = {6416, 6520, 6538, 6522, 6528, 6540, 6518, 6530, 6524, 6536, 6534, 6532, 6526, 5706, 14832}       -- Put all your id item pack in here
+maxbuy = 5                       -- Bot will try to buying X times if have more gems
+MinGems = 30000              -- Minimal Gems for Buying Pack
+
+IgnorePack = {}
+for _,pack in pairs(iditempack) do
+    if not pengecualian[pack] then
+        pengecualian[pack]=true
+    end
+    if not IgnorePack[pack] then
+        IgnorePack[pack] = true
+    end
+end
