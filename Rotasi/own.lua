b5c9eed840449d0a69dbf863a1df88db83a9e2b3
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2024-10-19 00:30:39 +0800

    Update own.lua

--- Rotasi/own.lua
+++ Rotasi/own.lua
@@ -12,18 +12,18 @@ delayrandomworld = 10000
 nLettPNB = 6
 Limit_BotInfo = 12
 PNBinFarm = false
-autobuypack = false
+autobuypack = true
 
 DelayRecon = 90000              -- Interval Reconnecting
 DelayBadServer = 120000
 
 ID_AcuanDrop_Pack = 13           -- ID Foreground or Background for Drop Pack
-hargapack = 15000                -- Pack Price
-namapack = "star_supplies"       -- Pack Name
-MinItemPack = 10                 -- Bot will dropping the pack when he have X MinItemPack or more
-iditempack = {6416, 6520, 6538, 6522, 6528, 6540, 6518, 6530, 6524, 6536, 6534, 6532, 6526, 5706, 14832}       -- Put all your id item pack in here
-maxbuy = 5                       -- Bot will try to buying X times if have more gems
-MinGems = 30000              -- Minimal Gems for Buying Pack
+hargapack = 5000                -- Pack Price
+namapack = "crime_wave"       -- Pack Name
+MinItemPack = 5                 -- Bot will dropping the pack when he have X MinItemPack or more
+iditempack = {2382, 2292, 2294, 2296, 2298, 2300, 2308, 2310, 2312, 2314, 2316, 2320, 2322, 2324, 2326, 2328, 2332, 2334, 2336, 2338, 2340}       -- Put all your id item pack in here
+maxbuy = 6                       -- Bot will try to buying X times if have more gems
+MinGems = 25000              -- Minimal Gems for Buying Pack
 
 IgnorePack = {}
 for _,pack in pairs(iditempack) do
