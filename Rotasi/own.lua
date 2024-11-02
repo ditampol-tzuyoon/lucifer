41db2bf196710204fc14732924569080cb64f3c5
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2024-11-02 16:13:33 +0800

    Update own.lua

--- Rotasi/own.lua
+++ Rotasi/own.lua
@@ -12,15 +12,20 @@ delayrandomworld = 10000
 nLettPNB = 6
 Limit_BotInfo = 12
 PNBinFarm = false
-autobuypack = false
-MinItemPack = 1
-namapack = "growganoth"
-MinGOG = 30
-MinGems = 45000
 
 DelayRecon = 75000
 DelayBadServer = 75000
-iditempack = {10322, 10324, 10326, 3102, 1238, 1220, 3116, 1988, 5256, 1198, 10328, 13996}
+
+autobuypack = true
+hargapack = 15000                -- Pack Price
+namapack = "star_supplies"       -- Pack Name
+MinItemPack = 75                 -- Bot will dropping the pack when he have X MinItemPack or more
+iditempack = {6416, 6520, 6538, 6522, 6528, 6540, 6518, 6530, 6524, 6536, 6534, 6532, 6526}
+maxbuy = 6                       -- Bot will try to buying X times if have more gems
+MinGems = 30000              -- Minimal Gems for Buying Pack
+
+EventLabu = false
+MinGOG = 30
 
 IgnorePack = {}
 for _,pack in pairs(iditempack) do
