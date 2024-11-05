c6a5d93fc031df85009cdd4ed0c42fb359c93635
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2024-11-06 00:48:37 +0800

    Update own.lua

--- Rotasi/own.lua
+++ Rotasi/own.lua
@@ -17,12 +17,12 @@ DelayRecon = 75000
 DelayBadServer = 75000
 
 autobuypack = true
-hargapack = 15000                -- Pack Price
-namapack = "star_supplies"       -- Pack Name
-MinItemPack = 25                 -- Bot will dropping the pack when he have X MinItemPack or more
-iditempack = {6416, 6520, 6538, 6522, 6528, 6540, 6518, 6530, 6524, 6536, 6534, 6532, 6526}
-maxbuy = 6                       -- Bot will try to buying X times if have more gems
-MinGems = 30000              -- Minimal Gems for Buying Pack
+hargapack = 45000                -- Pack Price
+namapack = "surg_value_pack"     -- Pack Name
+MinItemPack = 20                 -- Bot will dropping the pack when he have X MinItemPack or more
+iditempack = {1270, 4316, 4310, 1258, 4312, 4318, 4308, 1260, 1268, 4314, 1264, 1266, 1262, 4296}              -- Put all your id item pack in here
+maxbuy = 2                      -- Bot will try to buying X times if have more gems
+MinGems = 45000              -- Minimal Gems for Buying Pack
 
 EventLabu = false
 MinGOG = 30
