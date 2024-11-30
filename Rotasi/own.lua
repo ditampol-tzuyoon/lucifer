efd984d2fd0cd47bf77374d5133c8fdfaa0d9799
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2024-11-30 22:20:02 +0800

    Update own.lua

--- Rotasi/own.lua
+++ Rotasi/own.lua
@@ -17,13 +17,13 @@ DelayRecon = 150000
 DelayBadServer = 150000
 KillGhost = false
 
-autobuypack = false
-hargapack = 2000                -- Pack Price
-namapack = "signal_jammer"       -- Pack Name
-MinItemPack = 2                 -- Bot will dropping the pack when he have X MinItemPack or more
+autobuypack = true
+hargapack = 15000                -- Pack Price
+namapack = "star_supplies"       -- Pack Name
+MinItemPack = 25                 -- Bot will dropping the pack when he have X MinItemPack or more
 iditempack = {10394, 6416, 6520, 6538, 6522, 6528, 6540, 6518, 6530, 6524, 6536, 6534, 6532, 6526, 226}
-maxbuy = 190                       -- Bot will try to buying X times if have more gems
-MinGems = 2000              -- Minimal Gems for Buying Pack
+maxbuy = 7                       -- Bot will try to buying X times if have more gems
+MinGems = 15000              -- Minimal Gems for Buying Pack
 
 
 EventLabu = false
