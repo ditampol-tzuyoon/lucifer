8ec28177efe95fec6860361ea32265f082acb27a
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2024-12-03 09:20:38 +0800

    Update own.lua

--- Rotasi/own.lua
+++ Rotasi/own.lua
@@ -19,10 +19,10 @@ KillGhost = true
 
 autobuypack = true
 hargapack = 15000                -- Pack Price
-namapack = "thermo_blast"       -- Pack Name
+namapack = "star_supplies"       -- Pack Name
 MinItemPack = 1                 -- Bot will dropping the pack when he have X MinItemPack or more
 iditempack = {10394, 6416, 6520, 6538, 6522, 6528, 6540, 6518, 6530, 6524, 6536, 6534, 6532, 6526, 2978, 1402}
-maxbuy = 30                       -- Bot will try to buying X times if have more gems
+maxbuy = 7                       -- Bot will try to buying X times if have more gems
 MinGems = 15000              -- Minimal Gems for Buying Pack
 
 
