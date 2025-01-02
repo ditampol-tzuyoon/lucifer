7485ca75128522ff036b84f04984e556c171db6b
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2025-01-03 00:31:11 +0800

    Update own.lua

--- Rotasi/own.lua
+++ Rotasi/own.lua
@@ -3,7 +3,14 @@ ShowBotInfo = false
 DurasiWorldPNB = 80
 SetLevelWorld = 30
 
---[[
+hargapack = 20000                -- Pack Price
+namapack = "world_lock_10_pack"         -- Pack Name
+MinItemPack = 10                 -- Bot will dropping the pack when he have X MinItemPack or more
+iditempack = {5402, 10538, 10536, 242}              -- Put all your id item pack in here
+maxbuy = 5                      -- Bot will try to buying X times if have more gems
+MinGems = 20000
+
+
 IgnorePack = {}
 for _,pack in pairs(iditempack) do
     if not pengecualian[pack] then
@@ -13,6 +20,3 @@ for _,pack in pairs(iditempack) do
         IgnorePack[pack] = true
     end
 end
-]]--
-
-
