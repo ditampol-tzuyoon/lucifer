4aac64ccbea5e1bd5819fc032ca855e10b1b32d1
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2025-01-17 03:24:39 +0800

    Update own.lua

--- Rotasi/own.lua
+++ Rotasi/own.lua
@@ -2,12 +2,12 @@ ShowBotInfo = false
 DurasiWorldPNB = 80
 SetLevelWorld = 30
 
-hargapack = 20000                -- Pack Price
-namapack = "world_lock_10_pack"         -- Pack Name
-MinItemPack = 10                 -- Bot will dropping the pack when he have X MinItemPack or more
-iditempack = {5402, 10538, 10536, 242}              -- Put all your id item pack in here
-maxbuy = 5                      -- Bot will try to buying X times if have more gems
-MinGems = 20000
+-- hargapack = 20000                -- Pack Price
+-- namapack = "world_lock_10_pack"         -- Pack Name
+-- MinItemPack = 10                 -- Bot will dropping the pack when he have X MinItemPack or more
+-- iditempack = {5402, 10538, 10536, 242}              -- Put all your id item pack in here
+-- maxbuy = 5                      -- Bot will try to buying X times if have more gems
+-- MinGems = 20000
 
 -- if getUsername() == "oline5" then
 --     RestSchedule = false
@@ -15,6 +15,13 @@ MinGems = 20000
 --         "13.45 - 15.30"
 --     }
 -- end
+
+hargapack = 50000
+namapack = "mega_party_pack"
+MinItemPack = 1
+iditempack = {2306, 7672}
+maxbuy = 5
+MinGems = 50000
 
 IgnorePack = {}
 for _,pack in pairs(iditempack) do
