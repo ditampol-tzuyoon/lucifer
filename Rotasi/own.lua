49a6d2851b08f6d06e934bf6038ead9cdf4c8a28
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2025-03-03 05:48:47 +0800

    Update own.lua

--- Rotasi/own.lua
+++ Rotasi/own.lua
@@ -12,6 +12,13 @@ iditempack = {5402, 10538, 10536, 242, 5764}   -- Put all your id item pack in h
 maxbuy = 5                      -- Bot will try to buying X times if have more gems
 MinGems = 20000
 
+if getUsername() == "monna2" then
+    hargapack = 2000
+    namapack = "world_lock"
+    MinItemPack = 5
+    MinGems = 4000
+end
+
 -- hargapack = 10000                   -- Pack Price
 -- namapack = "cny_spray"               -- Pack Name
 -- MinItemPack = 2                    -- Bot will dropping the pack when he have X MinItemPack or more
