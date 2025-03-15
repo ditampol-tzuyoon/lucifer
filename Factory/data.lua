8cf6b04a3dc84430ad9f6563022365787633a548
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2025-03-15 14:21:04 +0800

    Update data.lua

--- Factory/data.lua
+++ Factory/data.lua
@@ -3,5 +3,6 @@ Data_User = {
     {nama="monna2", maxbot=24, expired=1744361832, userdc=422670528483033088},
     {nama="dangperma", maxbot=20, expired=9999999999, userdc=845154898797527060},
     {nama="bendot13pog", maxbot=10, expired=9999999999, userdc=1140675310157832303},
-    {nama="dimassetiadi74", maxbot=50, expired=9999999999, userdc=967097189904056341}
+    {nama="dimassetiadi74", maxbot=50, expired=9999999999, userdc=967097189904056341},
+    {nama="MasxyLIMIT4", maxbot=20, expired=1744611637, userdc=461383747393028096}
 }
