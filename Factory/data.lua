cdc7a891b24654ebc79ae323a9026be64797aaae
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2025-04-03 14:20:38 +0800

    Update data.lua

--- Factory/data.lua
+++ Factory/data.lua
@@ -8,5 +8,6 @@ Data_User = {
     {nama="vodnew1", maxbot=20, expired=9999999999, userdc=521002987540316184},
     {nama="rodricz1", maxbot=20, expired=1744869952, userdc=768118364681273385},
     {nama="LUCIEoTpiQvsKgYn", maxbot=30, expired=1745434450, userdc=574260465346019343},
-    {nama="noirtake", maxbot=104, expired=1745610763, userdc=232086070697787392}
+    {nama="noirtake", maxbot=104, expired=1745610763, userdc=232086070697787392},
+    {nama="KaiMannn", maxbot=10, expired=9999999999, userdc=883776144103579648}
 }
