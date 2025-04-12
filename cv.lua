c76f5914359d0faa792566b2f51d7cce47cd5d3e
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2025-04-12 12:15:29 +0800

    Update cv.lua

--- cv.lua
+++ cv.lua
@@ -27,6 +27,7 @@ function StatusBot(detail)
         [BotStatus.retrieving_token] = "Retrieving Token",
         [BotStatus.logon_fail] = "Login Fail",
         [BotStatus.error_connecting] = "Ercon",
+        [BotStatus.banwave_detected] = "Banwave",
     }
 
     local erS
